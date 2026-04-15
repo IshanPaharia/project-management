5fd646ed4087148d28cafc3fb4a2178281f63ac5
Ishan Paharia <ishanpaharia@gmail.com>
2026-04-15 13:30:53 +0530

    Added Task and Comments

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import wrouter from "./routes/workspaceRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
+import taskRouter from "./routes/taskRoutes.js";
+import { protect } from "./middleware/authMiddleware.js";
+import commentRouter from "./routes/commentRoutes.js";
 
 dotenv.config();
 
@@ -23,7 +26,9 @@ app.get("/", (req, res) => {
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Routes
-app.use("/api/workspaces", wrouter);
-app.use("/api/projects", projectRouter)
+app.use("/api/workspaces", protect, wrouter);
+app.use("/api/projects", protect, projectRouter)
+app.use("/api/task", protect, taskRouter)
+app.use("/api/comment", protect,commentRouter)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
